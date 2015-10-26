d1d4f43ce255cb582edfd27b1d5dd73a38593ad8
rev005 <revanth2009@gmail.com>
2015-10-26 02:12:18 -0500

    updated app styles.
    almost final

--- js/buttons.js
+++ js/buttons.js
@@ -7,14 +7,19 @@ $(document).ready(function(){
       $("#toggle-up").click(function(){
         $(".player-cover").toggle( );
       });
+
+      var audioArray = document.getElementsByClassName('playsong');
+      console.log(audioArray);
+      var nowPlaying = audioArray[0];
+      nowPlaying.load();
+
+      $('.play').on('click', function(){
+          nowPlaying.play();
+      });
       
+      $('.stop').on('click', function(){
+          nowPlaying.pause();
+      });
 	});
 
-/*function() {
-              $( ".player" ).css( "height":"100vh" );
-              $( ".player-cover" ).show();
-            }, 
-            function() {
-              $( ".player" ).css( "height":"auto" );
-              $( ".player-cover" ).hide();
-            }*/
+
